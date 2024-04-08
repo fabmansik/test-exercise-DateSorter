fba987580e2b36ef00969f59f24f5fbf96b0c9c9
fabmansik <milansomyk@gmail.com>
2024-04-08 12:13:38 +0300

    alternative version where used only one HashMap and improved performance

--- Program.java
+++ Program.java
@@ -16,10 +16,10 @@ public class Program {
         }
 //        (2004-07-01, 2005-01-02, 2007-01-01, 2032-05-03)
         List<LocalDate> test = new ArrayList<>();
-        test.add(LocalDate.of(2004,07,01));
-        test.add(LocalDate.of(2005,01,02));
-        test.add(LocalDate.of(2007,01,01));
-        test.add(LocalDate.of(2032,05,03));
+        test.add(LocalDate.of(2004, 7,1));
+        test.add(LocalDate.of(2005,1,2));
+        test.add(LocalDate.of(2007,1,1));
+        test.add(LocalDate.of(2032,5,3));
         Collection<LocalDate> localDates = dateSorter.sortDates(test);
         System.out.println(localDates);
     }
